3ccff5d5c3c346d45d6b44d072784a5e23475f32
ekir <ekir.gustafsson@gmail.com>
2015-04-30 13:12:29 +0200

    SPLASH

---
+++ app/src/main/java/com/example/ekir/project/MenuActivity.java
@@ -0,0 +1,55 @@
+package com.example.ekir.project;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+
+
+public class MenuActivity extends ActionBarActivity {
+    Button btn_play;
+    Button btn_about;
+    CheckBox chk_music;
+    DungeonCrawlerApplication app;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        app = (DungeonCrawlerApplication)getApplication();
+        app.init_music();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        btn_play= (Button) findViewById(R.id.btn_play);
+        btn_about= (Button) findViewById(R.id.btn_about);
+        chk_music = (CheckBox) findViewById(R.id.chk_music);
+        btn_about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                MainActivity.this.startActivity(intent);
+                */
+            }
+        });
+        chk_music.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                app.setMusic(isChecked);
+            }
+        });
+        btn_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*
+                Intent intent = new Intent(MainActivity.this, PlayActivity.class);
+                MainActivity.this.startActivity(intent);
+                */
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/ekir/project/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.ekir.project;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * This activity handles the splash screen
+ */
+public class SplashActivity extends ActionBarActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            final int wait_time = 2000;
+            setContentView(R.layout.activity_splash);
+
+            Handler handler = new Handler();
+
+            handler.postDelayed(new Runnable() {
+                public void run() {
+
+                    // make sure we close the splash screen so the user won't come back when it presses back key
+
+                    finish();
+                    Intent intent = new Intent(SplashActivity.this, MenuActivity.class);
+                    SplashActivity.this.startActivity(intent);
+                }
+
+            }, wait_time);
+        }
+    }
